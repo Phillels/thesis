c2820e655710ac7ac4d52e7f8c21e0d74f7bd0a4
Phillels <phillels@osuosl.org>
2016-04-24 16:58:58 -0700

    suggested model

--- eds.r
+++ eds.r
@@ -36,7 +36,7 @@ cor(projectstat$bugcount,projectstat$vulnerabilities)
 cor(projectstat$bugcount, projectstat$vulns)
 cor(projectstat$bd, projectstat$vulns)
 cor(projectstat$cocomo, projectstat$vulns)
-#As loc increases, vulnerabilities increases (.632)
+#As cocomo increases, vulnerabilities increases (.632)
 cor(projectstat$cocomo, projectstat$vulnerabilities)
 cor(projectstat$loc, projectstat$vulns)
 cor(projectstat$age, projectstat$vulnerabilities)
@@ -51,6 +51,8 @@ cor(projectstat$cocomo, projectstat$bd)
 plot(projectstat$vulnerabilities)
 plot(projectstat$cocomo)
 
+shapiro.test(projectstat$bd)
+
 qqnorm(vulnerabilities, main = 'Normal Probability Plot of Vulnerabilities')
 qqnorm(cocomo, main = 'Normal Probability Plot of Cocomo')
 
@@ -61,7 +63,53 @@ abline(lm(vulnerabilities ~ effort), col="red")
 lm.out=lm(vulnerabilities~cocomo)
 summary(lm.out)
 
-#t test **, positive
+lm.out=lm(vulns~cocomo)
+summary(lm.out)
+
+#!!!! r-squared .5331
+lm.out=lm(vulnerabilities~loc+totaldevs)
+summary(lm.out)
+
+#r-squared .543
+lm.out=lm(vulnerabilities~loc+activedevs)
+summary(lm.out)
+
+#!!! r-squared .5512
+totaleffort <-(projectstat$totaldevs*projectstat$yearcount)
+
+lm.out=lm(vulnerabilities~loc+totaleffort)
+summary(lm.out)
+
+
+
+currenteffort <-(projectstat$activedevs*projectstat$yearcount)
+
+lm.out=lm(vulns~cocomo)
+summary(lm.out)
+
+
+log.vulnerabilities <-log(vulnerabilities)
+hist(log.vulnerabilities)
+
+log.cocomo <-log(cocomo)
+hist(log.cocomo)
+
+
+root.vuln <- (vulnerabilities)^.5
+root.cocomo <- (cocomo)^.5
+
+
+hist(root.vuln)
+
+qqnorm(root.vuln)
+qqnorm(root.cocomo)
+qplot(trans.fitted, trans.resid) + geom_hline(yintercept = 0)
+qplot(trans.fitted, cocomo) + geom_hline(yintercept = 0)
+
+
+
+
+#t test *, positive
 lm.out1=lm(vulns~cocomo)
 summary(lm.out1)
 
